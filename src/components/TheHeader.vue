8bc11f030623109da3b388df18f4416af9f5244f
PaoloCrescenzi <crescenzipaolo2@gmail.com>
2023-02-17 16:24:31 +0100

    components

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Project</a>
+                <a class="nav-item nav-link" href="#">Chi siamo</a>
+                <a class="nav-item nav-link" href="#">Contatti</a>
+            </div>
+        </div>
+</nav>
+</template>
+
+<style lang="scss">
+@use "styles/main";
+@use "bootstrap/scss/bootstrap";
+</style>
---
+++ src/components/TheMain.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template></template>
+
+<style lang="scss"></style>
